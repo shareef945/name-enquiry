448320aa7fce87a4d991370b444fee68d39f2d06
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2023-03-23 11:44:12 +0000

    fixed auth

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 export default function Home() {
-  const [data, setData] = useState("hello");
+  const [data, setData] = useState("null");
   const router = useRouter();
   const callApi = async () => {
     try {
@@ -37,9 +37,97 @@ export default function Home() {
 
   useEffect(() => {
     callApi();
-    if (data) {
-      router.push("/name-enquiry");
-    }
-  }, [data]);
+  }, []);
+
+  return (
+    <>
+      {data ? (
+        <div className={styles.container}>
+          <Head>
+            <title>Ecobank Name Enquiry</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+
+          <h1 className={styles.title}>
+            <img className={styles.image} src="/portal/name-enquiry/images/ecobank.png" />
+          </h1>
+
+          <main>
+            <div className={styles.grid}>
+              <h1 className={styles.heading}>Account Name Enquiry</h1>
+              <form action="" method="post">
+              <TextareaAutosize
+                className={styles.textarea}
+                maxRows={4}
+                minRows={4}
+                placeholder="Institution ID | Account Number"
+                defaultValue=""
+                style={{ width: 300 }}
+              />
+              <button className={styles.button} type="submit">
+                Submit
+              </button>
+              </form>
+            </div>
+          </main>
+
+          <footer>Powered by Ecobank Ghana PLC</footer>
+
+          <style jsx>{`
+            main {
+              padding: 5rem 0;
+              flex: 1;
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+            }
+            footer {
+              width: 100%;
+              height: 100px;
+              border-top: 1px solid #eaeaea;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+            footer img {
+              margin-left: 0.5rem;
+            }
+            footer a {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              text-decoration: none;
+              color: inherit;
+            }
+            code {
+              background: #fafafa;
+              border-radius: 5px;
+              padding: 0.75rem;
+              font-size: 1.1rem;
+              font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+                DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New,
+                monospace;
+            }
+          `}</style>
+
+          <style jsx global>{`
+            html,
+            body {
+              padding: 0;
+              margin: 0;
+              font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                sans-serif;
+            }
+            * {
+              box-sizing: border-box;
+            }
+          `}</style>
+        </div>
+      ) : (
+        <div>Not logged in</div>
+      )}
+    </>
+  );
 }
-export { data };
